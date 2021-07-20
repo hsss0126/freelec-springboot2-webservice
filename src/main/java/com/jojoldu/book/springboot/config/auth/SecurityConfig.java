a42640bb434917d7aa0eb72e4b54d804367e6dcb
hyuns <hsss0126@gmail.com>
2021-07-20 19:18:48 +0900

    Google 로그인 연동

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // 모두 허용
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // USER권한 사용자만 허용
+                    .anyRequest().authenticated() // 그 외 요청은 인증된 사용자에게만 허용
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
